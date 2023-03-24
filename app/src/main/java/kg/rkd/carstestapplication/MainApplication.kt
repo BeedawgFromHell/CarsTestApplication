f4c036e9e22b9400c49369e5b2810552661d8daf
Kasymaly@sotkon.ru <Qwerty123!@>
2023-03-24 11:27:24 +0600

    di

--- app/src/main/java/kg/rkd/carstestapplication/MainApplication.kt
+++ app/src/main/java/kg/rkd/carstestapplication/MainApplication.kt
@@ -1,9 +1,19 @@
 package kg.rkd.carstestapplication
 
 import android.app.Application
+import kg.rkd.carstestapplication.di.koinModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 
 class MainApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(koinModule)
+        }
     }
 }
---
+++ app/src/main/java/kg/rkd/carstestapplication/di/module.kt
@@ -0,0 +1,8 @@
+package kg.rkd.carstestapplication.di
+
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val koinModule = module {
+
+}
