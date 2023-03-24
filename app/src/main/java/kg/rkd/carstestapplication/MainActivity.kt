b7fbed0152b8a95a94ad4fc5e3d3cfb29c0573d2
Kasymaly@sotkon.ru <Qwerty123!@>
2023-03-24 10:34:18 +0600

    added libs

--- app/src/main/java/kg/rkd/carstestapplication/MainActivity.kt
+++ app/src/main/java/kg/rkd/carstestapplication/MainActivity.kt
@@ -2,30 +2,17 @@ package kg.rkd.carstestapplication
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kg.rkd.carstestapplication.ui.theme.CarsTestApplicationTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            CarsTestApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+
+        setContentView(R.layout.main_activity)
     }
 }
 
